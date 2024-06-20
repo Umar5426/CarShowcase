a1e8da6dfc72c89ffd5a5752576f8dc194e42d88
Umar5426 <114004528+Umar5426@users.noreply.github.com>
2024-06-20 13:59:20 +0400

    1 project setup (#2)
    * Creating the Project
    
    * intital page setup
    
    * setting up Hero file
    
    * updating globals.css
    
    * updated tailwind config
    
    * updates to Hero
    
    * making a custom button
    
    * defining types of props for CustomButton
    
    * updating CustomButton and fixing intial page.ts
    
    * fixes in CustomButton
    
    * adding Public assets
    
    * adding image to hero
    
    * updating page layout
    
    ---------
    
    Co-authored-by: Umar Khan <muk779912@gmail.com>

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "CarHub",
+  description: "Discover the best cars in the world",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ components/Hero.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Image from "next/image"
+import CustomButton from "./CustomButton"
+
+const Hero = () => {
+
+    const handleScroll = () => {};
+  return (
+    <div className="hero">
+        <div className="flex-1 pt-36 padding-x">
+            <h1 className="hero_title">
+                Find, Book, or Rent a Car - Quickly and Easily
+            </h1>
+
+            <p className="hero__subtitle">
+                Streamline your car rental expirence with our efforless
+                booking process.
+            </p>
+
+            <CustomButton
+            title="Explore Cars"
+            containerStyles="bg-primary-blue
+            text-white rounded-full mt-16"
+            handleClick={handleScroll}
+            />
+
+        </div>
+
+        <div className="hero__image-container">
+            <div className="hero__image">
+                <Image src="/../public/hero.png" alt="hero"
+                fill className="object-container" />
+                <div className="hero__image-overlay" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
