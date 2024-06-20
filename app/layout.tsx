98fda4313bc81cd0f173c1aab615da1c51ace329
Umar Khan <muk779912@gmail.com>
2024-06-20 14:22:58 +0400

    creating NavBar and Footer components

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <NavBar/>
+        {children}
+        <Footer/>
+      </body>
     </html>
   );
 }
