0aac63d6a968e9a80905003176779d941e0bbca0
Umar5426 <114004528+Umar5426@users.noreply.github.com>
2024-06-20 16:29:19 +0400

    3 project layout (#4)
    * creating NavBar and Footer components
    
    * feat(NavBar): Adding navbar logic
    
    * feat(CustomButton): Dynamically identifying which type of button is needed, updating the interface and button logic
    
    * feat(Footer): Adding Footer component logic
    
    * feat(constants): Adding business information in constant's index
    
    * feat(Footer): Adding constants links into footer
    
    * fix(Styling): fixing some styling prompts. Making sure padding is applied properly as well as layout order
    
    ---------
    
    Co-authored-by: Umar Khan <muk779912@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Footer, NavBar } from "@/components";
 
 export const metadata: Metadata = {
   title: "CarHub",
@@ -13,7 +14,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <NavBar/>
+        {children}
+        <Footer/>
+      </body>
     </html>
   );
 }
