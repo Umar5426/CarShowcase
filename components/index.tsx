619162319703128d5133dd9ba330c7c069bdb8f7
Umar Khan <muk779912@gmail.com>
2024-06-20 18:45:16 +0400

    feat(SearchManufacturer): Adding logic for SearchManufacturer, and fixing a referencing error

--- components/index.tsx
+++ components/index.tsx
@@ -2,5 +2,9 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import CustomFilter from "./CustomFilter";
 
-export {Hero, CustomButton, NavBar, Footer};
+export {Hero, CustomButton, NavBar, Footer, SearchBar, 
+    SearchManufacturer, CustomFilter};
