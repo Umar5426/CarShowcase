bd1aefb0cfd0b40fd5498af01523cc656b67ce07
Umar Khan <muk779912@gmail.com>
2024-06-20 13:17:21 +0400

    updates to Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,20 @@ import Image from "next/image"
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="hero">
+        <div className="flex-1 pt-36 padding-x">
+            <h1 className="hero_title">
+                Find, Book, or Rent a Car - Quickly and Easily
+            </h1>
+
+            <p className="hero__subtitle">
+                Streamline your car rental expirence with our efforless
+                booking process.
+            </p>
+
+            <CustomButton/>
+        </div>
+    </div>
   )
 }
 
