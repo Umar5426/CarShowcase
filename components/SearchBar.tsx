eebf75a0e92d1569f5082018493ceb906b462d60
Umar Khan <muk779912@gmail.com>
2024-06-20 16:40:56 +0400

    feat(SearchManufacturer): incorperating SearchManufacturer componesnt into SearchBar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import SearchManufacturer from "./SearchManufacturer";
+import { useState } from "react";
 
 const SearchBar = () => {
+    const[manufacturer, setManufacturer] = useState('');
     const handleSearch = () => {}
   return (
     <form className='searchbar' onSubmit={handleSearch}>
         <div className="searchbar__item">
-            <SearchManufacturer/>
+            <SearchManufacturer
+            manufacturer={manufacturer}
+            setManufacturer={setManufacturer}/>
         </div>
     </form>
   )
--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,8 @@ export interface CustomButtonProps{
     handleClick?:
     MouseEventHandler<HTMLButtonElement>;
 }
+
+export interface SearchManufacturerProps{
+    manufacturer: string;
+    setManufacturer: (manufacturer: string) => void;
+}
